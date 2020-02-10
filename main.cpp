ffe7dd7df44ad2e05b10d5926b124b221a5e7196
Can <37780414+ccevik48@users.noreply.github.com>
2020-02-10 14:01:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,11 +9,7 @@
  *
  *
  *
- * This program is to act as a keylogger; a majority of this code was not written by me, but I cannot 
- * find the original author as this program was saved by me at least more than a year ago. However, 
- * changes were made to it by adding 'else if' instead of just 'if' for faster logic flow, and special
- * keys that are not alpha-numeric were also added. The program works in the background so you can 
- * have it running while the window is not highlighted and while minimized. 
+ * This program is to act as a keylogger; 
  */
 
 
